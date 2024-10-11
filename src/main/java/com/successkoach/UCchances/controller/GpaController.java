d83ca0305c8decb47d1e341fd7b3a552c6bd3390
Keshav Acharya <keshav.ach@outlook.com>
2024-10-10 18:47:24 -0700

    added api support for trimesters and semesters

--- src/main/java/com/successkoach/UCchances/controller/GpaController.java
+++ src/main/java/com/successkoach/UCchances/controller/GpaController.java
@@ -12,4 +12,18 @@ public class GpaController {
                 / (float) 100.00;
     }
 
+    @GetMapping("/findtrigpa")
+    public float gpaCalcTrimester(@RequestParam int a, @RequestParam int b, @RequestParam int c, @RequestParam int d,
+            @RequestParam int f, @RequestParam int honors) {
+        return Math.round((((float) a * 4 + b * 3 + c * 2 + d + Math.min(honors, 12) * 0.67f) / (a + b + c + d + f)) * 100)
+                / (float) 100.00;
+    }
+
+    @GetMapping("/findquartgpa")
+    public float gpaCalcQuarter(@RequestParam int a, @RequestParam int b, @RequestParam int c, @RequestParam int d,
+            @RequestParam int f, @RequestParam int honors) {
+        return Math.round((((float) a * 4 + b * 3 + c * 2 + d + Math.min(honors, 16) * 0.5f) / (a + b + c + d + f)) * 100)
+                / (float) 100.00;
+    }
+
 }
